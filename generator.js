966fff92a176e7342f1de2d693471b2339fcccab
ddanailov <dimityr.danailov@gmail.com>
2020-12-01 20:05:45 +0200

    Generator uses rounds to generate meaningful collection set.

--- generator.js
+++ generator.js
@@ -1,22 +1,27 @@
 const fs = require('fs')
 let writeStream = fs.createWriteStream('example.jsonl')
 
-const records = 1200000
-let i = 0
-while (i < records) {
-  i++
-  const object = {
-    id: i,
-    orders: randomNumber(),
-    orderDate: randomDate(new Date(2005, 0, 1), new Date()),
-    reviews: randomNumber(),
-    reviewDate: randomDate(new Date(2005, 0, 1), new Date()),
-    wishes: randomNumber(),
-    wishDate: randomDate(new Date(2005, 0, 1), new Date()),
-  }
+const totalRecords = 1200000
+const rounds = 10000
+const records = totalRecords / rounds
 
-  const newLine = `\n`
-  writeStream.write(`${JSON.stringify(object)}${newLine}`)
+for (let j = 0; j < rounds; j++) {
+  let i = 0
+  while (i < records) {
+    i++
+    const object = {
+      productId: i,
+      orders: randomNumber(),
+      orderDate: randomDate(new Date(2005, 0, 1), new Date()),
+      reviews: randomNumber(),
+      reviewDate: randomDate(new Date(2005, 0, 1), new Date()),
+      wishes: randomNumber(),
+      wishDate: randomDate(new Date(2005, 0, 1), new Date()),
+    }
+
+    const newLine = `\n`
+    writeStream.write(`${JSON.stringify(object)}${newLine}`)
+  }
 }
 
 writeStream.on('finish', () => {
